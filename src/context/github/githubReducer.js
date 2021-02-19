893122d38e9753cc64246bb241bc4bf54eae6933
Rustam Talapov <rustam_talapov@epam.com>
2021-02-19 10:49:22 +0200

    Layout bug fixed

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -8,9 +8,9 @@ import {
 
 const handlers = {
   [SEARCH_USERS]: (state, {payload}) => ({...state, users: payload, loading: false}),
-  [GET_REPOS]: (state, payload) => ({...state, repos: payload, loading: false}),
-  [GET_USER]: (state, payload) => ({...state, user: payload, loading: false}),
-  [SET_LOADING]: (state) => ({...state, loading: true}),
+  [GET_REPOS]: (state, {payload}) => ({...state, repos: payload, loading: false}),
+  [GET_USER]: (state, {payload}) => ({...state, user: payload, loading: false}),
+  [SET_LOADING]: state => ({...state, loading: true}),
   [CLEAR_USERS]: state => ({...state, users: []}),
   DEFAULT: state => state
 };
